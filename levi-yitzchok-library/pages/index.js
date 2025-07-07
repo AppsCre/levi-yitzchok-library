818fc619c3a51ef4eb8f7cbdaf3d27a43c5fa770
AppsCre <appscreative01@gmail.com>
2025-07-07 16:04:32 -0400

    Update index.js

--- levi-yitzchok-library/pages/index.js
+++ levi-yitzchok-library/pages/index.js
@@ -9,13 +9,13 @@ import { Book, User, Calendar, DollarSign, Search, Plus, Edit, Trash2, LogIn, Lo
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqsnCNPR3O4W9nUfzJorGX3kdHnRWPI9Y",
-  authDomain: "levi-yitzchok-library.firebaseapp.com",
-  databaseURL: "https://levi-yitzchok-library-default-rtdb.firebaseio.com",
-  projectId: "levi-yitzchok-library",
-  storageBucket: "levi-yitzchok-library.firebasestorage.app",
-  messagingSenderId: "1000072970171",
-  appId: "1:1000072970171:web:b9858668b45ee40581ce9e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
